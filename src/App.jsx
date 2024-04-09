871521dd389745bd897e9a2f3fc54c9e42336cb0
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-04-09 16:53:24 +0200

    toggle todo

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,18 @@ export default function App(){
     setNewItem('')
   }
 
+  function toggleTodo(id, completed){
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id){
+          return{ ...todo, completed}
+        }
+
+        return todo
+      })
+    })
+  }
+
   console.log(todos)
 
   return (
@@ -37,7 +49,8 @@ export default function App(){
             return(
             <li key={todo.id}>
               <label >
-                  <input type="checkbox" checked={todo.completed}/>
+                  <input type="checkbox" checked={todo.completed} 
+                  onChange={e => toggleTodo(todo.id, e.target.checked)}/>
                   {todo.title}
               </label>
               <button className="btn btn-danger">DELETE</button>
