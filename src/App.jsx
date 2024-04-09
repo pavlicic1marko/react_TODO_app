0ac71113ec97dcfafd5b7568f0a7f37ffc87df92
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-04-09 13:13:20 +0200

    adding new item form

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,19 @@ import "./styles.css"
 
 export default function App(){
   return (
-  <h1 className="header">Todo List</h1>
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label>New Item</label>
+          <input value='' type="text" />
+          <button className="btn">Create</button>
+        </div>
+      </form>
+
+      <h1 className="header">Todo List</h1>
+      <ul>
+        <li>TEST</li>
+      </ul>
+    </>
   )
 }
