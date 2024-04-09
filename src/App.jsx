680a2fcf7054383366bce85ecfd1ba3e8f1c8a60
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-04-09 15:02:33 +0200

    save item to todos

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,25 @@ import "./styles.css"
 export default function App(){
 
   const [newItem,setNewItem] = useState('')
+  const [todos,setTodos] = useState([])
+
+  function handelSubmit(e){
+    e.preventDefault()
+
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos, 
+        {id: crypto.randomUUID(), title: newItem, completed:
+           false },
+          ]
+    })
+  }
+
+  console.log(todos)
 
   return (
     <>
-      <form className="new-item-form">
+      <form onSubmit={handelSubmit} className="new-item-form">
         <div className="form-row">
           <label>New Item</label>
           <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" />
